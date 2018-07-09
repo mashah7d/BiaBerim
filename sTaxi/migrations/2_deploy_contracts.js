fe0e0ef2ed6547e7a2e52cbf1c3fefec7c30133d
mashah7d <mashahsavand@gmail.com>
2018-07-09 19:01:11 +0430

    initialization

---
+++ sTaxi/migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+var MainContract = artifacts.require("./MainContract.sol");
+var Trip = artifacts.require("./Trip.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(MainContract);
+  deployer.deploy(Trip);
+};
+
